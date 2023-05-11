8497317c8dbb0306ad068000cde303a27accdca4
Momen Ahmed Abdelhay <117471310+Momen-Ahmed74@users.noreply.github.com>
2023-05-11 23:40:40 +0300

    Create rest of activites

---
+++ StudentZone/app/src/main/java/com/studentzone/AdminDoctorsAccountsActivity.java
@@ -0,0 +1,14 @@
+package com.studentzone;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AdminDoctorsAccountsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_doctors_accounts);
+    }
+}
