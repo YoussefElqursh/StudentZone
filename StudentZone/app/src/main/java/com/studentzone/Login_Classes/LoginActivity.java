82fdb541dfbe76a2ac55d87810e7263ab932c754
Momen Ahmed Abdelhay <117471310+Momen-Ahmed74@users.noreply.github.com>
2023-05-14 05:28:46 +0300

    login update

---
+++ StudentZone/app/src/main/java/com/studentzone/Login_Classes/LoginActivity.java
@@ -0,0 +1,21 @@
+package com.studentzone.Login_Classes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+import com.studentzone.Admin_Classes.AdminHomeActivity;
+import com.studentzone.R;
+
+public class LoginActivity extends AppCompatActivity {
+    Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        button = findViewById(R.id.activity_login_btn_login);
+        button.setOnClickListener(v -> startActivity(new Intent(LoginActivity.this, AdminHomeActivity.class)));
+    }
+}
---
+++ StudentZone/app/src/main/java/com/studentzone/Welcome_Classes/WelcomeActivity.java
@@ -0,0 +1,23 @@
+package com.studentzone.Welcome_Classes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+import com.studentzone.Admin_Classes.AdminDepartmentsActivity;
+import com.studentzone.Admin_Classes.AdminHomeActivity;
+import com.studentzone.Login_Classes.LoginActivity;
+import com.studentzone.R;
+
+public class WelcomeActivity extends AppCompatActivity {
+    Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+        button = findViewById(R.id.activity_welcome_btn_welcome);
+        button.setOnClickListener(v -> startActivity(new Intent(WelcomeActivity.this, LoginActivity.class)));
+    }
+}
