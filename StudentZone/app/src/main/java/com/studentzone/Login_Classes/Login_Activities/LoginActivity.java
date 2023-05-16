919cce4c2fea156bc09e7e7b8da39755cc9f8b63
Momen Ahmed Abdelhay <117471310+Momen-Ahmed74@users.noreply.github.com>
2023-05-16 15:43:36 +0300

    Login update

--- StudentZone/app/src/main/java/com/studentzone/Login_Classes/Login_Activities/LoginActivity.java
+++ StudentZone/app/src/main/java/com/studentzone/Login_Classes/Login_Activities/LoginActivity.java
@@ -3,14 +3,19 @@ package com.studentzone.Login_Classes.Login_Activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 import com.studentzone.Admin_Calsses.Admin_Activities.AdminHomeActivity;
 import com.studentzone.R;
 
 public class LoginActivity extends AppCompatActivity {
     Button button;
+    RadioButton radioButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
