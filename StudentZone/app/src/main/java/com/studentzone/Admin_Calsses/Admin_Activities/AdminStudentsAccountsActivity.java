5f8cc9e61975c9cac2ea83d525cb1b14849acebf
Momen Ahmed Abdelhay <117471310+Momen-Ahmed74@users.noreply.github.com>
2023-05-17 20:48:55 +0300

    Updates

--- StudentZone/app/src/main/java/com/studentzone/Admin_Calsses/Admin_Activities/AdminStudentsAccountsActivity.java
+++ StudentZone/app/src/main/java/com/studentzone/Admin_Calsses/Admin_Activities/AdminStudentsAccountsActivity.java
@@ -7,26 +7,29 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 
 import com.google.android.material.bottomsheet.BottomSheetDialog;
 import com.studentzone.R;
 
 public class AdminStudentsAccountsActivity extends AppCompatActivity {
-    Button activity_admin_students_accounts_btn_add, activity_admin_students_accounts_btn_back ;
+    Button activity_admin_students_accounts_btn_add, activity_admin_students_accounts_btn_back;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_students_accounts);
-        showBottomSheetDialog();
+        showAndHideBottomSheetDialog();
         buttonBackAction();
     }
 
-    public void showBottomSheetDialog() {
+    public void showAndHideBottomSheetDialog() {
         activity_admin_students_accounts_btn_add = findViewById(R.id.activity_admin_students_accounts_btn_add);
         activity_admin_students_accounts_btn_add.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+
                 BottomSheetDialog bottomSheetDialog = new BottomSheetDialog(AdminStudentsAccountsActivity.this, R.style.BottomSheetStyle);
                 View bottomSheetDialogView = LayoutInflater.from(getApplicationContext()).inflate(R.layout.fragment_admin_new_student_account, (LinearLayout)findViewById(R.id.fragment_admin_new_student_ll_main));
                 bottomSheetDialogView.findViewById(R.id.fragment_admin_new_student_btn_close).setOnClickListener(new View.OnClickListener() {
--- StudentZone/app/src/main/java/com/studentzone/Admin_Calsses/Admin_Fragments/AdminNewStudentAccountFragment.java
+++ StudentZone/app/src/main/java/com/studentzone/Admin_Calsses/Admin_Fragments/AdminNewStudentAccountFragment.java
@@ -7,22 +7,16 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 import com.studentzone.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link AdminNewStudentAccountFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class AdminNewStudentAccountFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    EditText et_studet_name;
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
 
@@ -30,15 +24,6 @@ public class AdminNewStudentAccountFragment extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment AdminNewStudentAccountFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static AdminNewStudentAccountFragment newInstance(String param1, String param2) {
         AdminNewStudentAccountFragment fragment = new AdminNewStudentAccountFragment();
         Bundle args = new Bundle();
@@ -58,9 +43,10 @@ public class AdminNewStudentAccountFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+
+
         return inflater.inflate(R.layout.fragment_admin_new_student_account, container, false);
     }
 }
