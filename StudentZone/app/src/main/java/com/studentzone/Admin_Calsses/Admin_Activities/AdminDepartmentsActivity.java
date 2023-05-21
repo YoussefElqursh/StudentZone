2adbed022b0c7551ad13c0fa20fe0ff22025f764
Youssef Ramadan <79144849+youssef883@users.noreply.github.com>
2023-05-21 09:15:54 +0300

    Update AdminDepartmentsActivity.java

--- StudentZone/app/src/main/java/com/studentzone/Admin_Calsses/Admin_Activities/AdminDepartmentsActivity.java
+++ StudentZone/app/src/main/java/com/studentzone/Admin_Calsses/Admin_Activities/AdminDepartmentsActivity.java
@@ -1,102 +1,63 @@
 package com.studentzone.Admin_Calsses.Admin_Activities;
-import android.widget.EditText;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
-import com.google.android.material.bottomsheet.BottomSheetDialog;
-import com.studentzone.Admin_Calsses.Admin_Models.Admin_Department_Model.AdminDepartmentAdaper;
-import com.studentzone.Admin_Calsses.Admin_Models.Admin_Department_Model.AdminDepartmentModel;
-import com.studentzone.Data_Base.My_DB;
+import android.widget.Toast;
+
+import com.studentzone.Admin_Calsses.Admin_Models.Admin_Subject_Model.AdminSubjectAdapter;
+import com.studentzone.Admin_Calsses.Admin_Models.Admin_Subject_Model.AdminSubjectModel;
+import com.studentzone.Admin_Calsses.Admin_Models.Admin_Subject_Model.SelectListener;
 import com.studentzone.R;
+
 import java.util.ArrayList;
 import java.util.List;
 
-public class AdminDepartmentsActivity extends AppCompatActivity  {
-    My_DB db = new My_DB(this);
-    Button btn_add, btn_back, btm_sheet_dia_btn_save, btm_sheet_dia_btn_close;
-    EditText btm_sheet_dia_et_dept_name, btm_sheet_dia_et_dept_code;
-    BottomSheetDialog bottomSheetDialog;
-
+public class AdminAbsenceFilesActivity extends AppCompatActivity implements SelectListener {
+    Button btn_back;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_admin_departments);
-
-
-        btn_add = findViewById(R.id.activity_admin_depatrments_btn_add);
-        bottomSheetDialog = new BottomSheetDialog(AdminDepartmentsActivity.this, R.style.BottomSheetStyle);
-        buttonAddAction();
-
-        btn_add.setOnClickListener(new View.OnClickListener()
-        {
-            public void onClick(View view)
-            {
-                bottomSheetDialog.show();
-            }
-        });
+        setContentView(R.layout.activity_admin_absence_files);
         buttonBackAction();
-
-        ArrayList<String> dept_name = db.get_departments_name();
-        ArrayList<String> dept_code = db.get_departments_code();
-
-        DoctorDepartmentRecyclerView(dept_name, dept_code);
+        DoctorAbsenceFileRecyclerView();
     }
-
-    // This function to show and hide bottomSheetDialog //
-    public void buttonAddAction() {
-        View bottomSheetDialogView = getLayoutInflater().inflate(R.layout.fragment_admin_new_department,null, false);
-
-        btm_sheet_dia_btn_save = bottomSheetDialogView.findViewById(R.id.fragment_new_department_btn_save);
-        btm_sheet_dia_btn_close = bottomSheetDialogView.findViewById(R.id.fragment_admin_new_department_btn_close);
-
-        btm_sheet_dia_et_dept_name = bottomSheetDialogView.findViewById(R.id.fragment_new_department_et_course_name);
-        btm_sheet_dia_et_dept_code = bottomSheetDialogView.findViewById(R.id.fragment_new_department_et_course_code);
-
-        // the action of save button to add new department in bottom sheet
-        btm_sheet_dia_btn_save.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String name = btm_sheet_dia_et_dept_name.getText().toString();
-                String code = btm_sheet_dia_et_dept_code.getText().toString();
-
-                db.insert_department(name, code);
-
-                bottomSheetDialog.dismiss();
-            }
-        });
-
-        // the action of close button in bottom sheet
-        btm_sheet_dia_btn_close.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                bottomSheetDialog.dismiss();
-            }
-        });
-        bottomSheetDialog.setContentView(bottomSheetDialogView);
-    }
-
-    //the button to return to admin home page
     public void buttonBackAction(){
-        btn_back = findViewById(R.id.activity_admin_departments_btn_back);
-        btn_back.setOnClickListener(v -> startActivity(new Intent(AdminDepartmentsActivity.this,AdminHomeActivity.class)));
+        btn_back = findViewById(R.id.activity_admin_absence_files_btn_back);
+        btn_back.setOnClickListener(v -> startActivity(new Intent(AdminAbsenceFilesActivity.this,AdminHomeActivity.class)));
+    }
+    public RecyclerView DoctorAbsenceFileRecyclerView()
+    {
+        RecyclerView recyclerView = findViewById(R.id.activity_admin_absence_file_subject_recyclerview);
+
+        List<AdminSubjectModel> subjectModel = new ArrayList<AdminSubjectModel>();
+        subjectModel.add(new AdminSubjectModel("Operating System-1","260",R.drawable.ic_book_1));
+        subjectModel.add(new AdminSubjectModel("Software Engineering-1","1120",R.drawable.ic_book_1));
+        subjectModel.add(new AdminSubjectModel("Software Engineering-2","500",R.drawable.ic_book_1));
+        subjectModel.add(new AdminSubjectModel("Multmedia","3",R.drawable.ic_book_1));
+        subjectModel.add(new AdminSubjectModel("Computer Programming-1","8",R.drawable.ic_book_1));
+        subjectModel.add(new AdminSubjectModel("os","bad",R.drawable.ic_book_1));
+        subjectModel.add(new AdminSubjectModel("os","bad",R.drawable.ic_book_1));
+        subjectModel.add(new AdminSubjectModel("os","bad",R.drawable.ic_book_1));
+        subjectModel.add(new AdminSubjectModel("os","bad",R.drawable.ic_book_1));
+        subjectModel.add(new AdminSubjectModel("os","bad",R.drawable.ic_book_1));
+        subjectModel.add(new AdminSubjectModel("os","bad",R.drawable.ic_book_1));
+        subjectModel.add(new AdminSubjectModel("os","bad",R.drawable.ic_book_1));
+        subjectModel.add(new AdminSubjectModel("os","bad",R.drawable.ic_book_1));
+        subjectModel.add(new AdminSubjectModel("os","bad",R.drawable.ic_book_1));
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(new AdminSubjectAdapter(getApplicationContext(),subjectModel, this));
+        return recyclerView;
     }
 
-    // the view data to show the departments to admin
-    public RecyclerView DoctorDepartmentRecyclerView(ArrayList<String> name, ArrayList<String> code) {
-        RecyclerView recyclerView = findViewById(R.id.activity_admin_departments_recycelerview);
-
-        List<AdminDepartmentModel> admindepartmentModel = new ArrayList<AdminDepartmentModel>();
-
-        for (int i = 0; i <name.size() ; i++)
-        {
-            admindepartmentModel.add(new AdminDepartmentModel(name.get(i), code.get(i),R.drawable.ic_department_1));
-        }
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        recyclerView.setAdapter(new AdminDepartmentAdaper(getApplicationContext(),admindepartmentModel));
-        return recyclerView;
+    @Override
+    public void onItemClicked(AdminSubjectModel adminSubjectModel) {
+        Toast.makeText(this, adminSubjectModel.getSubjectName(), Toast.LENGTH_SHORT).show();
     }
 }
