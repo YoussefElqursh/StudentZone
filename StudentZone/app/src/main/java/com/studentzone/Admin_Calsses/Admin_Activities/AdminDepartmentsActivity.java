2f63ed199a5ff2da3da55d0b2a7ef805c976d334
AliAhmedHasan <117413566+AliAhmedHasan@users.noreply.github.com>
2023-05-24 00:53:06 +0300

    stable reporistory

--- StudentZone/app/src/main/java/com/studentzone/Admin_Calsses/Admin_Activities/AdminDepartmentsActivity.java
+++ StudentZone/app/src/main/java/com/studentzone/Admin_Calsses/Admin_Activities/AdminDepartmentsActivity.java
@@ -1,34 +1,21 @@
 package com.studentzone.Admin_Calsses.Admin_Activities;
-
-import android.annotation.SuppressLint;
 import android.widget.EditText;
 import androidx.appcompat.app.AppCompatActivity;
-
-
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.SearchView;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 import com.google.android.material.bottomsheet.BottomSheetDialog;
 import com.studentzone.Admin_Calsses.Admin_Models.Admin_Department_Model.departmentRecyclerViewAdapter;
 import com.studentzone.Data_Base.Departments;
 import com.studentzone.Data_Base.My_DB;
-import android.widget.Toast;
-
-import com.studentzone.Admin_Calsses.Admin_Models.Admin_Subject_Model.AdminSubjectAdapter;
-import com.studentzone.Admin_Calsses.Admin_Models.Admin_Subject_Model.AdminSubjectModel;
-import com.studentzone.Admin_Calsses.Admin_Models.Admin_Subject_Model.SelectListener;
-
 import com.studentzone.R;
-
 import java.util.ArrayList;
+import java.util.List;
 
 public class AdminDepartmentsActivity extends AppCompatActivity  {
     My_DB db = new My_DB(this);
@@ -36,87 +23,64 @@ public class AdminDepartmentsActivity extends AppCompatActivity  {
     EditText btm_sheet_dia_et_dept_name, btm_sheet_dia_et_dept_code;
     BottomSheetDialog bottomSheetDialog;
 
-
-    @SuppressLint("MissingInflatedId")
-
-public class AdminAbsenceFilesActivity extends AppCompatActivity implements SelectListener {
-    Button btn_back;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_admin_absence_files);
+        setContentView(R.layout.activity_admin_departments);
+
+
+        btn_add = findViewById(R.id.activity_admin_depatrments_btn_add);
+        bottomSheetDialog = new BottomSheetDialog(AdminDepartmentsActivity.this, R.style.BottomSheetStyle);
+        buttonAddAction();
+
+        btn_add.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View view)
+            {
+                bottomSheetDialog.show();
+            }
+        });
         buttonBackAction();
-
         showAllDepartments();
-
     }
 
     // This function to show and hide bottomSheetDialog //
     public void buttonAddAction() {
         View bottomSheetDialogView = getLayoutInflater().inflate(R.layout.fragment_admin_new_department,null, false);
 
-        btm_sheet_dia_btn_save = bottomSheetDialogView.findViewById(R.id.fragment_new_department_btn_save);
-        btm_sheet_dia_btn_close = bottomSheetDialogView.findViewById(R.id.fragment_admin_new_department_btn_close);
+        Button btn_save = bottomSheetDialogView.findViewById(R.id.fragment_new_department_btn_save);
 
         btm_sheet_dia_et_dept_name = bottomSheetDialogView.findViewById(R.id.fragment_new_department_et_course_name);
         btm_sheet_dia_et_dept_code = bottomSheetDialogView.findViewById(R.id.fragment_new_department_et_course_code);
 
-        // the action of save button to add new department in bottom sheet
-        btm_sheet_dia_btn_save.setOnClickListener(new View.OnClickListener() {
+        btn_save.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Toast.makeText(getBaseContext(), "Saved Successfully ✔️", Toast.LENGTH_SHORT).show();
+
                 String name = btm_sheet_dia_et_dept_name.getText().toString();
                 String code = btm_sheet_dia_et_dept_code.getText().toString();
 
                 db.insert_department(name, code);
 
+                showAllDepartments();
+
                 btm_sheet_dia_et_dept_name.setText("");
                 btm_sheet_dia_et_dept_code.setText("");
 
-                showAllDepartments();
-                bottomSheetDialog.dismiss();
-            }
-        });
-
-        // the action of close button in bottom sheet
-        btm_sheet_dia_btn_close.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
                 bottomSheetDialog.dismiss();
             }
         });
         bottomSheetDialog.setContentView(bottomSheetDialogView);
-        DoctorAbsenceFileRecyclerView();
-
     }
+
     public void buttonBackAction(){
-        btn_back = findViewById(R.id.activity_admin_absence_files_btn_back);
-        btn_back.setOnClickListener(v -> startActivity(new Intent(AdminAbsenceFilesActivity.this,AdminHomeActivity.class)));
+        btn_back = findViewById(R.id.activity_admin_departments_btn_back);
+        btn_back.setOnClickListener(v -> startActivity(new Intent(AdminDepartmentsActivity.this,AdminHomeActivity.class)));
     }
-    public RecyclerView DoctorAbsenceFileRecyclerView()
-    {
-        RecyclerView recyclerView = findViewById(R.id.activity_admin_absence_file_subject_recyclerview);
-
-        List<AdminSubjectModel> subjectModel = new ArrayList<AdminSubjectModel>();
-        subjectModel.add(new AdminSubjectModel("Operating System-1","260",R.drawable.ic_book_1));
-        subjectModel.add(new AdminSubjectModel("Software Engineering-1","1120",R.drawable.ic_book_1));
-        subjectModel.add(new AdminSubjectModel("Software Engineering-2","500",R.drawable.ic_book_1));
-        subjectModel.add(new AdminSubjectModel("Multmedia","3",R.drawable.ic_book_1));
-        subjectModel.add(new AdminSubjectModel("Computer Programming-1","8",R.drawable.ic_book_1));
-        subjectModel.add(new AdminSubjectModel("os","bad",R.drawable.ic_book_1));
-        subjectModel.add(new AdminSubjectModel("os","bad",R.drawable.ic_book_1));
-        subjectModel.add(new AdminSubjectModel("os","bad",R.drawable.ic_book_1));
-        subjectModel.add(new AdminSubjectModel("os","bad",R.drawable.ic_book_1));
-        subjectModel.add(new AdminSubjectModel("os","bad",R.drawable.ic_book_1));
-        subjectModel.add(new AdminSubjectModel("os","bad",R.drawable.ic_book_1));
-        subjectModel.add(new AdminSubjectModel("os","bad",R.drawable.ic_book_1));
-        subjectModel.add(new AdminSubjectModel("os","bad",R.drawable.ic_book_1));
-        subjectModel.add(new AdminSubjectModel("os","bad",R.drawable.ic_book_1));
 
 
-    public void showAllDepartments()
-    {
+    public void showAllDepartments() {
         My_DB myDb = new My_DB(getBaseContext());
 
         ArrayList<Departments> departmentsArrayList = db.showDepartments();
@@ -124,16 +88,11 @@ public class AdminAbsenceFilesActivity extends AppCompatActivity implements Sele
         departmentRecyclerViewAdapter adapter = new departmentRecyclerViewAdapter(departmentsArrayList);
         RecyclerView.LayoutManager lm = new LinearLayoutManager(this);
 
-        RecyclerView rv  = findViewById(R.id.activity_admin_departments_recycelerview);
+        RecyclerView rv = findViewById(R.id.activity_admin_departments_recycelerview);
 
         rv.setHasFixedSize(true);
         rv.setLayoutManager(lm);
         rv.setAdapter(adapter);
 
-    }
-
-    @Override
-    public void onItemClicked(AdminSubjectModel adminSubjectModel) {
-        Toast.makeText(this, adminSubjectModel.getSubjectName(), Toast.LENGTH_SHORT).show();
     }
 }
--- StudentZone/app/src/main/java/com/studentzone/Admin_Calsses/Admin_Activities/AdminDoctorsAccountsActivity.java
+++ StudentZone/app/src/main/java/com/studentzone/Admin_Calsses/Admin_Activities/AdminDoctorsAccountsActivity.java
@@ -6,86 +6,222 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 import com.google.android.material.bottomsheet.BottomSheetDialog;
-import com.studentzone.Admin_Calsses.Admin_Models.Admin_Doctors_Accounts_Model.AdminDoctorAccountAdapter;
-import com.studentzone.Admin_Calsses.Admin_Models.Admin_Doctors_Accounts_Model.AdminDoctorAccountModel;
-import com.studentzone.Admin_Calsses.Admin_Models.Admin_Subject_and_AbcenceFile_Model.AdminSubjectAdapter;
-import com.studentzone.Admin_Calsses.Admin_Models.Admin_Subject_and_AbcenceFile_Model.AdminSubjectModel;
+//import com.studentzone.Admin_Calsses.Admin_Models.Admin_Doctor_Model.doctorRecyclerViewAdapter;
+//import com.studentzone.Admin_Calsses.Admin_Models.Admin_Student_Model.studentRecyclerViewAdapter;
+import com.studentzone.Data_Base.Doctors;
+import com.studentzone.Data_Base.My_DB;
 import com.studentzone.R;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class AdminDoctorsAccountsActivity extends AppCompatActivity {
-    Button btn_add, btn_back;
+
+
+    BottomSheetDialog bottomSheetDialog;
+    View bottomSheetDialogView;
+    Button btn_add, btn_back, btm_sheet_dia_btn_save, btm_sheet_dia_btn_close;
+    EditText btm_sheet_dia_et_doctor_name, btm_sheet_dia_et_doctor_password, btm_sheet_dia_et_doctor_email;
+
+    RadioGroup btm_sheet_dia_rg;
+    RadioButton btm_sheet_dia_rb_male, btm_sheet_dia_rb_female;
+
+    String name, email, password, gender;
+    RecyclerView rv;
+
+    int kindCheckedId = -1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_doctors_accounts);
+
+        inflate();
+        showAllDoctors();
         buttonAddAction();
+//        radioButtonGroupAction();
+        saveDoctorData();
+        closeBottomSheet();
         buttonBackAction();
-        AdminDoctorAccountsRecyclerView();
+
+
     }
 
-    // This function to show and hide bottomSheetDialog //
-    public void buttonAddAction() {
+    /**inflate
+     **********************************************************************************************/
+    public void inflate() {
         btn_add = findViewById(R.id.activity_admin_doctors_accounts_btn_add);
+        btn_back = findViewById(R.id.activity_admin_doctors_accounts_btn_back);
+
+        bottomSheetDialog = new BottomSheetDialog(AdminDoctorsAccountsActivity.this, R.style.BottomSheetStyle);
+        bottomSheetDialogView = getLayoutInflater().inflate(R.layout.fragment_admin_new_doctor_account, null, false);
+
+        btm_sheet_dia_btn_save = bottomSheetDialogView.findViewById(R.id.fragment_new_doctor_btn_save);
+        btm_sheet_dia_btn_close = bottomSheetDialogView.findViewById(R.id.fragment_admin_new_doctor_btn_close);
+
+        btm_sheet_dia_et_doctor_name = bottomSheetDialogView.findViewById(R.id.fragment_new_doctor_et_doctor_name);
+        btm_sheet_dia_et_doctor_password = bottomSheetDialogView.findViewById(R.id.fragment_new_doctor_et_doctor_password);
+        btm_sheet_dia_et_doctor_email = bottomSheetDialogView.findViewById(R.id.fragment_new_doctor_et_doctor_email);
+
+        RadioGroup btm_sheet_dia_rg = bottomSheetDialogView.findViewById(R.id.fragment_new_doctor_rg_doctor_kind);
+        btm_sheet_dia_rb_male = bottomSheetDialogView.findViewById(R.id.fragment_new_doctor_rb_male);
+        btm_sheet_dia_rb_female = bottomSheetDialogView.findViewById(R.id.fragment_new_doctor_rb_female);
+
+//        rv = findViewById(R.id.activity_admin_doctors_accounts_rv);
+    }
+
+
+    /**show Bottom Sheet Dialog()
+     **********************************************************************************************/
+    public void buttonAddAction() {
         btn_add.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                BottomSheetDialog bottomSheetDialog = new BottomSheetDialog(AdminDoctorsAccountsActivity.this, R.style.BottomSheetStyle);
-                View bottomSheetDialogView = LayoutInflater.from(getApplicationContext()).inflate(R.layout.fragment_admin_new_doctor_account, (LinearLayout)findViewById(R.id.fragment_admin_new_doctor_ll_main));
-                bottomSheetDialogView.findViewById(R.id.fragment_admin_new_doctor_btn_close).setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        bottomSheetDialog.dismiss();
-                    }
-                });
-                bottomSheetDialogView.findViewById(R.id.fragment_new_doctor_btn_save).setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        bottomSheetDialog.dismiss();
-                    }
-                });
-                bottomSheetDialog.setContentView(bottomSheetDialogView);
-                bottomSheetDialog.show();
+
                 bottomSheetDialog.setContentView(bottomSheetDialogView);
                 bottomSheetDialog.show();
             }
         });
     }
+
+
+    /** close Bottom Sheet Dialog()
+     **********************************************************************************************/
+    public void closeBottomSheet() {
+        btm_sheet_dia_btn_close.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                bottomSheetDialog.dismiss();
+                nullEditTexts();
+                showAllDoctors();
+            }
+        });
+    }
+
+    /**check Validation Of Entered Data And save It()
+     **********************************************************************************************/
+    public void saveDoctorData() {
+        btm_sheet_dia_btn_save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                name = btm_sheet_dia_et_doctor_name.getText().toString().trim();
+                email = btm_sheet_dia_et_doctor_email.getText().toString().trim();
+                password = btm_sheet_dia_et_doctor_password.getText().toString().trim();
+
+
+                if (TextUtils.isEmpty(name)) {
+                    btm_sheet_dia_et_doctor_name.setError("Is Required !");
+                    return;
+                }
+                if (TextUtils.isEmpty(email)) {
+                    btm_sheet_dia_et_doctor_email.setError("Is Required !");
+                    return;
+                }
+                if (!emailShouldEndsWithEdu(email)) {
+                    btm_sheet_dia_et_doctor_email.setError("Should End With .edu");
+                    return;
+                }
+                if (TextUtils.isEmpty(password)) {
+                    btm_sheet_dia_et_doctor_password.setError("Is Required !");
+                    return;
+                }
+
+//                addNewDoctor();
+                nullEditTexts();
+
+            }
+        });
+    }
+
+
+    /**Back To The Previous Activity()
+     **********************************************************************************************/
     public void buttonBackAction(){
         btn_back = findViewById(R.id.activity_admin_doctors_accounts_btn_back);
-        btn_back.setOnClickListener(v -> startActivity(new Intent(AdminDoctorsAccountsActivity.this,AdminHomeActivity.class)));
+        btn_back.setOnClickListener(v -> startActivity(new Intent(getBaseContext(),AdminHomeActivity.class)));
     }
 
-    public RecyclerView AdminDoctorAccountsRecyclerView()
-    {
-        RecyclerView recyclerView = findViewById(R.id.activity_admin_doctors_accounts_recyclerview);
 
-        List<AdminDoctorAccountModel> adminDoctorAccountModel = new ArrayList<AdminDoctorAccountModel>();
-        adminDoctorAccountModel.add(new AdminDoctorAccountModel("os","bad",R.drawable.ic_male_doctor));
-        adminDoctorAccountModel.add(new AdminDoctorAccountModel("os","bad",R.drawable.ic_male_doctor));
-        adminDoctorAccountModel.add(new AdminDoctorAccountModel("os","bad",R.drawable.ic_male_doctor));
-        adminDoctorAccountModel.add(new AdminDoctorAccountModel("os","bad",R.drawable.ic_male_doctor));
-        adminDoctorAccountModel.add(new AdminDoctorAccountModel("os","bad",R.drawable.ic_male_doctor));
-        adminDoctorAccountModel.add(new AdminDoctorAccountModel("os","bad",R.drawable.ic_male_doctor));
-        adminDoctorAccountModel.add(new AdminDoctorAccountModel("os","bad",R.drawable.ic_male_doctor));
-        adminDoctorAccountModel.add(new AdminDoctorAccountModel("os","bad",R.drawable.ic_male_doctor));
-        adminDoctorAccountModel.add(new AdminDoctorAccountModel("os","bad",R.drawable.ic_male_doctor));
-        adminDoctorAccountModel.add(new AdminDoctorAccountModel("os","bad",R.drawable.ic_male_doctor));
-        adminDoctorAccountModel.add(new AdminDoctorAccountModel("os","bad",R.drawable.ic_male_doctor));
-        adminDoctorAccountModel.add(new AdminDoctorAccountModel("os","bad",R.drawable.ic_male_doctor));
-        adminDoctorAccountModel.add(new AdminDoctorAccountModel("os","bad",R.drawable.ic_male_doctor));
+    /**add New Doctor
+     **********************************************************************************************/
+//    public void addNewDoctor() {
+//
+//        My_DB db = new My_DB(getBaseContext());
+//
+//        name = btm_sheet_dia_et_doctor_name.getText().toString().trim();
+//        gender = "Male";
+//        email = btm_sheet_dia_et_doctor_email.getText().toString().trim();
+//        password = btm_sheet_dia_et_doctor_password.getText().toString().trim();
+//
+//
+//        Doctors doctor = new Doctors(name,gender,email,password);
+//        boolean added = db.addDoctor(doctor);
+//
+//        if(added){
+//            Toast.makeText(getBaseContext(), "Dr : "+doctor.getFName().toString()+" Is Successfully Saved ✔️", Toast.LENGTH_SHORT).show();
+//            bottomSheetDialog.dismiss();
+//        }
+//
+//        showAllDoctors();
+//    }
 
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        recyclerView.setAdapter(new AdminDoctorAccountAdapter(getApplicationContext(),adminDoctorAccountModel));
-        return recyclerView;
+
+    /**
+     * Special Validation For Email To End With .edu
+     **********************************************************************************************/
+    public static boolean emailShouldEndsWithEdu(String input) {
+        return TextUtils.isEmpty(input) ? false : input.endsWith(".edu");
     }
 
+
+    /**FillOut  EditTexts After Add New Doctor Or After Close Bottom Sheet Dialog()
+     **********************************************************************************************/
+    public void nullEditTexts() {
+
+        btm_sheet_dia_et_doctor_name.setText("");
+        btm_sheet_dia_et_doctor_email.setText("");
+        btm_sheet_dia_et_doctor_password.setText("");
+    }
+
+    /**Show All Doctors
+     **********************************************************************************************/
+    public void showAllDoctors() {
+
+        My_DB db = new My_DB(getBaseContext());
+
+
+//        ArrayList<Doctors> doctorsArrayList = db.showDoctors();
+
+//        doctorRecyclerViewAdapter adapter = new doctorRecyclerViewAdapter(doctorsArrayList);
+        RecyclerView.LayoutManager lm = new LinearLayoutManager(this);
+
+        rv.setHasFixedSize(true);
+        rv.setLayoutManager(lm);
+//        rv.setAdapter(adapter);
+    }
+
+
+    /**radioButtonGroupAction
+     *kindCheckedId, Get Index Of Checked User Kind in
+     ******************************************************************************************/
+    public void radioButtonGroupAction(){
+        btm_sheet_dia_rg.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                kindCheckedId = group.indexOfChild(findViewById(checkedId));
+//                Toast.makeText(AdminDoctorsAccountsActivity.this, kindCheckedId, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
 }
