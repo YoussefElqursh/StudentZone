41c0ae036cf913967f7f6c16ee141f93a2261390
Momen Ahmed Abdelhay <117471310+Momen-Ahmed74@users.noreply.github.com>
2023-07-31 02:40:25 +0300

    updates

--- StudentZone/app/src/main/java/com/studentzone/Login_Classes/Login_Activities/LoginActivity.java
+++ StudentZone/app/src/main/java/com/studentzone/Login_Classes/Login_Activities/LoginActivity.java
@@ -33,12 +33,12 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_welcome);
+        setContentView(R.layout.activity_login);
 
         inflate();
-//        sharedPreferencesFileCreation();
-//        radioButtonGroupAction();
-//        buttonLoginAction();
+        sharedPreferencesFileCreation();
+        radioButtonGroupAction();
+        buttonLoginAction();
     }
 
     /** Inflate
