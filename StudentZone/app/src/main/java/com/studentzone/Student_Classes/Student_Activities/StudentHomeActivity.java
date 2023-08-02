570c43ae3b43091ef675bcd45e91e07953837a7e
Ahmed Shosha <ashosha763@gmail.com>
2023-08-02 20:48:13 +0300

    logout in StudentHomeActivity.java

--- StudentZone/app/src/main/java/com/studentzone/Student_Classes/Student_Activities/StudentHomeActivity.java
+++ StudentZone/app/src/main/java/com/studentzone/Student_Classes/Student_Activities/StudentHomeActivity.java
@@ -8,7 +8,7 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.cardview.widget.CardView;
 import androidx.drawerlayout.widget.DrawerLayout;
 
-import android.app.Dialog;
+
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -25,18 +25,21 @@ import com.studentzone.Doctor_Classes.Doctor_Activities.DoctorHomeActivity;
 import com.studentzone.Login_Classes.Login_Activities.LoginActivity;
 import com.studentzone.R;
 
-public class StudentHomeActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+
+
+public class StudentHomeActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     CardView cv_subjects_registration, cv_subjects_passed_subjects, cv_subjects;
     Button btn_logout, btn_profile;
     SharedPreferences preferences;
     SharedPreferences.Editor editor;
-    TextView profileName ;
+    TextView profileName;
     DrawerLayout drawerLayout;
     NavigationView navigationView;
     ActionBarDrawerToggle toggle;
     Toolbar toolbar;
     Menu menu;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -50,9 +53,10 @@ public class StudentHomeActivity extends AppCompatActivity implements Navigation
     }
 
 
-    /** Inflate
+    /**
+     * Inflate
      **********************************************************************************************/
-    public void inflate(){
+    public void inflate() {
         cv_subjects_registration = findViewById(R.id.activity_student_home_cv_subjects_registration);
         cv_subjects_passed_subjects = findViewById(R.id.activity_student_home_cv_subjects_passed_subjects);
         cv_subjects = findViewById(R.id.activity_student_home_cv_subjects);
@@ -62,7 +66,7 @@ public class StudentHomeActivity extends AppCompatActivity implements Navigation
         toolbar = findViewById(R.id.activity_student_home_tb);
     }
 
-    public void subjectsRegistrationCardViewClickAction(){
+    public void subjectsRegistrationCardViewClickAction() {
         cv_subjects_registration.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -71,7 +75,7 @@ public class StudentHomeActivity extends AppCompatActivity implements Navigation
         });
     }
 
-    public void subjectsPerviousRequestCardViewClickAction(){
+    public void subjectsPerviousRequestCardViewClickAction() {
         cv_subjects_passed_subjects.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -80,27 +84,31 @@ public class StudentHomeActivity extends AppCompatActivity implements Navigation
         });
     }
 
-    public void subjectsCardViewClickAction(){
+
+
+    public void subjectsCardViewClickAction() {
         cv_subjects.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(StudentHomeActivity.this,StudentSubjectActivity.class));
+                startActivity(new Intent(StudentHomeActivity.this, StudentSubjectActivity.class));
             }
         });
     }
 
-    /**All Card Views Actions
+    /**
+     * All Card Views Actions
      **********************************************************************************************/
-    public void AllCardViewActions(){
+    public void AllCardViewActions() {
         subjectsRegistrationCardViewClickAction();
         subjectsPerviousRequestCardViewClickAction();
         subjectsCardViewClickAction();
     }
 
-    /**logOutConfirmationDialog()
+    /**
+     * logOutConfirmationDialog()
      * Shows a confirmation dialog when the user clicks on the log out button.
      * If the user confirms the log out action, call the logOut() method.
-     * **********************************************************************************************/
+     **********************************************************************************************/
     public void logOutConfirmationDialog() {
 
         btn_logout.setOnClickListener(new View.OnClickListener() {
@@ -122,9 +130,10 @@ public class StudentHomeActivity extends AppCompatActivity implements Navigation
         });
     }
 
-    /**logOut()
+    /**
+     * logOut()
      **********************************************************************************************/
-    private void logOut(){
+    private void logOut() {
         preferences = getSharedPreferences("Login_Prefs", MODE_PRIVATE);
         editor = preferences.edit();
         editor.clear();
@@ -141,26 +150,26 @@ public class StudentHomeActivity extends AppCompatActivity implements Navigation
 //        profileName = findViewById(R.id.activity_student_home_tv_profileName);
 
 
-        preferences = getSharedPreferences("userName",MODE_PRIVATE);
+        preferences = getSharedPreferences("userName", MODE_PRIVATE);
         String name = preferences.getString("fName", "");
 
         String capitalizedStr = name.substring(0, 1).toUpperCase() + name.substring(1);
         profileName.setText(capitalizedStr);
         btn_profile.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v)
-            {
-                if(profileName.getVisibility() == View.VISIBLE)
+            public void onClick(View v) {
+                if (profileName.getVisibility() == View.VISIBLE)
                     profileName.setVisibility(View.INVISIBLE);
                 else
                     profileName.setVisibility(View.VISIBLE);
             }
         });
     }
+
     /**
      * drawerToggleButtonAction()
      **********************************************************************************************/
-    public void drawerToggleButtonAction(){
+    public void drawerToggleButtonAction() {
         setSupportActionBar(toolbar);
         menu = navigationView.getMenu();
         navigationView.getHeaderView(0);
@@ -170,9 +179,39 @@ public class StudentHomeActivity extends AppCompatActivity implements Navigation
         toggle.syncState();
         getSupportActionBar().setHomeButtonEnabled(true);
         navigationView.setNavigationItemSelectedListener(this);
+
+
     }
+void logoutconfirmation2(){
+    AlertDialog.Builder builder = new AlertDialog.Builder(StudentHomeActivity.this);
+    builder.setTitle("Log Out");
+    builder.setMessage("Are you sure you want to log out?");
+    builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+        @Override
+        public void onClick(DialogInterface dialog, int which) {
+            logOut();
+        }
+    });
+    builder.setNegativeButton("No", null);
+    builder.show();
+
+}
+
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-        return false;
+
+      switch (item.getItemId()){
+          case R.id.activity_student_home_item_logout :
+              logoutconfirmation2();
+
+      }
+
+
+
+        return true;
     }
+
+
 }
+
+
