6478eb4740d46b343b6c2320cabae4d91dc014b3
KarimMohamed6 <117863289+KarimMohamed6@users.noreply.github.com>
2023-08-16 03:20:21 +0300

    edite image in doctor

--- StudentZone/app/src/main/java/com/studentzone/Admin_Classes/Admin_Activities/AdminHomeActivity.java
+++ StudentZone/app/src/main/java/com/studentzone/Admin_Classes/Admin_Activities/AdminHomeActivity.java
@@ -14,6 +14,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
@@ -50,6 +51,8 @@ public class   AdminHomeActivity extends AppCompatActivity implements Navigation
         fillOutAdminInfo();
         drawerToggleButtonAction();
     }
+
+
 
     /**
      * Inflate
@@ -101,6 +104,8 @@ public class   AdminHomeActivity extends AppCompatActivity implements Navigation
 //                break;
 //        }
     }
+
+
 
     /**
      * logOut()
