c53640f43c20fd47a270fcd3b47664f917ef677e
KarimMohamed6 <117863289+KarimMohamed6@users.noreply.github.com>
2023-08-25 17:19:51 +0300

    Set User Image And Name In SettingsActivity (admin,doctor,student)

--- StudentZone/app/src/main/java/com/studentzone/Doctor_Classes/Doctor_Activities/DoctorSettingsActivity.java
+++ StudentZone/app/src/main/java/com/studentzone/Doctor_Classes/Doctor_Activities/DoctorSettingsActivity.java
@@ -1,15 +1,25 @@
 package com.studentzone.Doctor_Classes.Doctor_Activities;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+
+import com.studentzone.Admin_Classes.Admin_Activities.AdminProfileActivity;
 import com.studentzone.R;
 
 public class DoctorSettingsActivity extends AppCompatActivity {
 
-    Button btn_back;
+    private Button btn_back;
+    private ImageView userImage;
+    private static final int PROFILE_IMAGE_REQUEST_CODE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +27,8 @@ public class DoctorSettingsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_doctor_settings);
 
         setBackButtonAction();
+        setUserNameAndImage();
+        openEditePersonalInfo();
     }
 
     /** setBackButtonAction()
@@ -25,5 +37,52 @@ public class DoctorSettingsActivity extends AppCompatActivity {
     public void setBackButtonAction(){
         btn_back = findViewById(R.id.activity_doctor_settings_btn_back);
         btn_back.setOnClickListener(v -> startActivity(new Intent(getBaseContext(), DoctorHomeActivity.class)));
+    }
+    /** setUserNameAndImage()
+     **********************************************************************************************/
+    public void setUserNameAndImage(){
+
+        SharedPreferences preferences = getSharedPreferences("userInfo",MODE_PRIVATE);
+        String name = preferences.getString("fName", "");
+        String image_uri = preferences.getString("image_uri", "");
+
+
+        TextView tv_userName = findViewById(R.id.profileName_tv);
+        userImage = findViewById(R.id.profile_IV);
+
+        tv_userName.setText(name);
+        userImage.setImageURI(Uri.parse(image_uri));
+
+    }
+
+    /** openEditePersonalInfo()
+     *  Opens the Edit Personal Info screen when the profile card is clicked.
+     **********************************************************************************************/
+    public void openEditePersonalInfo()
+    {
+        CardView cv_profile = findViewById(R.id.activity_doctor_settings_cv_profile);
+        cv_profile.setOnClickListener(v -> {
+            Intent intent = new Intent(DoctorSettingsActivity.this, AdminProfileActivity.class);
+
+            intent.putExtra("request_code", PROFILE_IMAGE_REQUEST_CODE);
+            startActivityForResult(intent, PROFILE_IMAGE_REQUEST_CODE);
+        });
+    }
+    /* this methode receive edited image from AdminProfileActivity ,
+     * to reflect the changes in DoctorSettingsActivity at the same time
+     **********************************************************************************************/
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == PROFILE_IMAGE_REQUEST_CODE && resultCode == RESULT_OK) {
+
+            assert data != null;
+            String updatedImageUri = data.getStringExtra("image_uri");
+
+            // Update the image in DoctorSettingsActivity using the received imageUri
+            userImage.setImageURI(Uri.parse(updatedImageUri));
+
+        }
     }
 }
--- StudentZone/app/src/main/java/com/studentzone/Student_Classes/Student_Activities/StudentSettingsActivity.java
+++ StudentZone/app/src/main/java/com/studentzone/Student_Classes/Student_Activities/StudentSettingsActivity.java
@@ -1,24 +1,33 @@
 package com.studentzone.Student_Classes.Student_Activities;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-import com.studentzone.Admin_Classes.Admin_Activities.AdminHomeActivity;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+
+import com.studentzone.Admin_Classes.Admin_Activities.AdminProfileActivity;
 import com.studentzone.R;
 
 public class StudentSettingsActivity extends AppCompatActivity {
 
     Button btn_back;
-
+    private ImageView userImage;
+    private static final int PROFILE_IMAGE_REQUEST_CODE = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_settings);
 
         setBackButtonAction();
+        setUserNameAndImage();
+        openEditePersonalInfo();
     }
 
     /** setBackButtonAction()
@@ -27,5 +36,54 @@ public class StudentSettingsActivity extends AppCompatActivity {
     public void setBackButtonAction(){
         btn_back = findViewById(R.id.activity_student_settings_btn_back);
         btn_back.setOnClickListener(v -> startActivity(new Intent(getBaseContext(), StudentHomeActivity.class)));
+    }
+
+    /** setUserNameAndImage()
+     **********************************************************************************************/
+    public void setUserNameAndImage(){
+
+        SharedPreferences preferences = getSharedPreferences("userInfo",MODE_PRIVATE);
+        String name = preferences.getString("fName", "");
+        String image_uri = preferences.getString("image_uri", "");
+
+
+        TextView tv_userName = findViewById(R.id.profileName_tv);
+        userImage = findViewById(R.id.profile_IV);
+
+        tv_userName.setText(name);
+        userImage.setImageURI(Uri.parse(image_uri));
+
+    }
+
+    /** openEditePersonalInfo()
+     *  Opens the Edit Personal Info screen when the profile card is clicked.
+     **********************************************************************************************/
+    public void openEditePersonalInfo()
+    {
+        CardView cv_profile = findViewById(R.id.activity_admin_settings_cv_profile);
+        cv_profile.setOnClickListener(v -> {
+            Intent intent = new Intent(StudentSettingsActivity.this, AdminProfileActivity.class);
+
+            intent.putExtra("request_code", PROFILE_IMAGE_REQUEST_CODE);
+            startActivityForResult(intent, PROFILE_IMAGE_REQUEST_CODE);
+        });
+    }
+
+    /* this methode receive edited image from AdminProfileActivity ,
+     * to reflect the changes in StudentSettingsActivity at the same time
+     **********************************************************************************************/
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == PROFILE_IMAGE_REQUEST_CODE && resultCode == RESULT_OK) {
+
+            assert data != null;
+            String updatedImageUri = data.getStringExtra("image_uri");
+
+            // Update the image in StudentSettingsActivity using the received imageUri
+            userImage.setImageURI(Uri.parse(updatedImageUri));
+
+        }
     }
 }
